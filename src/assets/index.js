0c262476424591448704b69ffce693262b5421c5
Busa Balázs <fishbalazs@gmail.com>
2024-03-01 13:01:36 +0100

    optimised the pictures

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,24 +2,11 @@ import menu from "./bars-staggered-solid.svg";
 import close from "./xmark-solid.svg";
 import facebook from "./facebook.svg";
 import phone from "./phone-solid.svg";
-import heroImg from "./pic_2.jpg";
-import pricesOneImg from "./pic_1.jpg";
-import pricesTwoImg from "./pic_4.jpg";
-import pricesThreeImg from "./pic_5.jpg";
-import pricesFourImg from "./pic_6.jpg";
-import contactImg from "./pic_5.jpg";
-import introduceImg from "./zsele.jpg";
+
 
 export {
   menu,
   close,
   facebook,
   phone,
-  heroImg,
-  contactImg,
-  introduceImg,
-  pricesOneImg,
-  pricesTwoImg,
-  pricesThreeImg,
-  pricesFourImg,
 };
